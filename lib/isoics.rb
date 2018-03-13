f3b7d598489caaa531d24a24e6974071fa17cfa2
Andrei Kislichenko <android.2net@gmail.com>
2018-03-13 19:07:39 +0100

    first commit

---
+++ lib/isoics.rb
@@ -0,0 +1,5 @@
+require "isoics/version"
+
+module Isoics
+  # Your code goes here...
+end
---
+++ spec/isoics_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Isoics do
+  it "has a version number" do
+    expect(Isoics::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
