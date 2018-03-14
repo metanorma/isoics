f7c695d17a4832953d64764ab5dcc1aeee25e11d
Andrei Kislichenko <android.2net@gmail.com>
2018-03-14 13:39:06 +0100

    update iso ics gem

--- spec/isoics_spec.rb
+++ spec/isoics_spec.rb
@@ -3,7 +3,23 @@ RSpec.describe Isoics do
     expect(Isoics::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "fetch field 01" do
+    ics = Isoics.fetch fieldcode: "01"
+    expect(ics).to be_instance_of Isoics::ICS
+    expect(ics.code).to eq "01"
+  end
+
+  it "fetch field 01, group 140" do
+    ics = Isoics.fetch fieldcode: "01", groupcode: "140"
+    expect(ics).to be_instance_of Isoics::ICS
+    expect(ics.groupcode).to eq "140"
+    expect(ics.notes).to be_instance_of Array
+    expect(ics.notes.first).to be_instance_of Isoics::Note
+  end
+
+  it "fetch field 01, group 140, subgroup 30" do
+    ics = Isoics.fetch fieldcode: "01", groupcode: "140", subgroupcode: "30"
+    expect(ics).to be_instance_of Isoics::ICS
+    expect(ics.subgroupcode).to eq "30"
   end
 end
